00ffe7f42b8d8f18de4edb220f2e6bae663c4f12
User <iliangonzalez2021@itp.edu.co>
2025-10-23 13:33:28 -0500

    Creacion del header

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+// Creamos nuestro functional component
+function Header() {
+
+    return (
+        <>
+            <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+                Sistema de Pacientes {''}
+                <span className="text-red-500">Veterinaria</span>
+            </h1>
+        </>
+    );
+}
+
+// Importamos nuestro functional component
+export default Header;
